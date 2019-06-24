f7a5f224fb642a4e1b1e17356816e03c69e6c723
Kryštof Rainisch <stofi@users.noreply.github.com>
2019-06-24 13:34:37 +0100

    add additional button

--- blinkr.ino
+++ blinkr.ino
@@ -1,22 +1,27 @@
 #include "functions.h"
 
 Button button_left = getButton(8,3); 
-Button button_right = getButton(7,2); 
+Button button_right = getButton(7,2);
+Button button = getButton(0,0);
 
 void setup() {
   buttonSetup(button_left);
   buttonSetup(button_right);
+  buttonSetup(button);
 }
 
 void loop() {
   buttonRead(button_left);
   buttonRead(button_right);
+  buttonRead(button);
 
   lightBlink(button_left.light);
   lightBlink(button_right.light);
 
+
   buttonsCompare(button_left, button_right);
 
+  digitalWrite(button.light.pin, button.light.state);
   digitalWrite(button_left.light.pin, button_left.light.blink_state);
   digitalWrite(button_right.light.pin, button_right.light.blink_state);
 }
